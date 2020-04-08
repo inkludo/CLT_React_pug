65f2e735d8e34824d1be6d661707e3c1c424865a
Vladyslav Lytvynenko <vlad.lytvynenko12@gmail.com>
2020-04-08 20:44:56 +0300

    added chart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 import Layout from './hoc/Layout/Layout';
-import ItemDetails from './containers/ItemDetails/ItemDetails';
 import { Classroom } from './containers/Classroom/Classroom';
 import { ClassroomList } from './containers/ClassroomList/ClassroomList';
+import { ItemDetails } from './containers/ItemDetails/ItemDetails';
 
 
 
--- src/containers/ItemDetails/ItemDetails.js
+++ src/containers/ItemDetails/ItemDetails.js
@@ -1,20 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+import { useHttp } from '../../hooks/http.hook'
+import { Loader } from '../../components/Loader/Loader'
+import { Chart } from '../../components/Chart/Chart'
 
-export default class ItemDetails extends React.Component {
+export const ItemDetails = (props) => {
 
-    state = {
-        float: { "f": [{ "t": 1585484661.9659426, "c": 88.56, "r": 26.56, "d": [{ "n": "some_disk_name", "p": 36.12 }] }, { "t": 1585484661.9659426, "c": 89.33, "r": 4.84, "d": [{ "n": "some_disk_name", "p": 36.68 }] }, { "t": 1585484661.9659426, "c": 95.27, "r": 61.38, "d": [{ "n": "some_disk_name", "p": 32.0 }] }, { "t": 1585484661.9659426, "c": 42.63, "r": 93.4, "d": [{ "n": "some_disk_name", "p": 2.29 }] }, { "t": 1585484661.9659426, "c": 36.89, "r": 65.04, "d": [{ "n": "some_disk_name", "p": 87.65 }] }, { "t": 1585484661.9659426, "c": 24.52, "r": 18.85, "d": [{ "n": "some_disk_name", "p": 76.18 }] }, { "t": 1585484661.9659426, "c": 66.96, "r": 33.61, "d": [{ "n": "some_disk_name", "p": 18.01 }] }, { "t": 1585484661.9659426, "c": 9.76, "r": 32.52, "d": [{ "n": "some_disk_name", "p": 25.57 }] }, { "t": 1585484661.9659426, "c": 35.7, "r": 46.14, "d": [{ "n": "some_disk_name", "p": 32.06 }] }, { "t": 1585484661.9659426, "c": 72.51, "r": 42.58, "d": [{ "n": "some_disk_name", "p": 24.63 }] }] }
+
+
+    const someData = [{ "t": 1585484661.9659426, "c": 88.56, "r": 26.56, "d": [{ "n": "some_disk_name", "p": 36.12 }] }, { "t": 1585484661.9659426, "c": 89.33, "r": 4.84, "d": [{ "n": "some_disk_name", "p": 36.68 }] }, { "t": 1585484661.9659426, "c": 95.27, "r": 61.38, "d": [{ "n": "some_disk_name", "p": 32.0 }] }, { "t": 1585484661.9659426, "c": 42.63, "r": 93.4, "d": [{ "n": "some_disk_name", "p": 2.29 }] }, { "t": 1585484661.9659426, "c": 36.89, "r": 65.04, "d": [{ "n": "some_disk_name", "p": 87.65 }] }, { "t": 1585484661.9659426, "c": 24.52, "r": 18.85, "d": [{ "n": "some_disk_name", "p": 76.18 }] }, { "t": 1585484661.9659426, "c": 66.96, "r": 33.61, "d": [{ "n": "some_disk_name", "p": 18.01 }] }, { "t": 1585484661.9659426, "c": 9.76, "r": 32.52, "d": [{ "n": "some_disk_name", "p": 25.57 }] }, { "t": 1585484661.9659426, "c": 35.7, "r": 46.14, "d": [{ "n": "some_disk_name", "p": 32.06 }] }, { "t": 1585484661.9659426, "c": 72.51, "r": 42.58, "d": [{ "n": "some_disk_name", "p": 24.63 }] }]
+
+    const state = {
+        time: [],
+        cpu: [],
+        ram: [],
+        disk: []
     }
 
-    render() {
-        console.log(this.props)
-        return (
-            <div style={{ textAlign: 'center' }}>
-                <h1>Hello from ItemDetails</h1>
-                <h2>ID: {this.props.match.params.id}</h2>
-            </div>
-        )
+
+
+
+
+    const [data, setData] = useState(null)
+
+    const { request, loading } = useHttp()
+
+
+    const fetchPcInfo = useCallback(async (state) => {
+        try {
+            //const fetched = await request(`localhost/getComputer`, 'GET', { k: { a: id } })
+            const fetched = [someData]
+            //this place for structuring data 
+            setData(fetched)
+        } catch (e) { console.log(e) }
+    }, [request])
+
+
+    useEffect(() => {
+        fetchPcInfo(state)
+    }, [fetchPcInfo])
+
+
+    if (loading) {
+        return <Loader />
     }
+
+
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>Hello from ItemDetails</h1>
+            <h2>ID: {props.match.params.id}</h2>
+            <Chart />
+        </div>
+    )
+
 }
 
 
